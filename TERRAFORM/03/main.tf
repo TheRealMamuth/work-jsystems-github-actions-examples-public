9d798a82d86606ac1fca1783bff9ef3f59ff74f7
Piotr Koska <piotr.s.koska@gmail.com>
2026-03-19 23:30:44 +0100

    Update

--- TERRAFORM/03/main.tf
+++ TERRAFORM/03/main.tf
@@ -53,6 +53,12 @@ resource "digitalocean_firewall" "main" {
     source_addresses = ["0.0.0.0/0"]
   }
 
+  inbound_rule {
+    protocol = "tcp"
+    port_range = "8080"
+    source_addresses = ["0.0.0.0/0"]
+  }
+
   outbound_rule {
     protocol              = "udp"
     port_range            = "1-65535"
