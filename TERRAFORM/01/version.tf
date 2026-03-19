bdd9ebf3d7f7f587af4ecdad4af5b72f5439aa83
Piotr Koska <piotr.s.koska@gmail.com>
2026-03-19 21:05:54 +0100

    Update

--- TERRAFORM/01/version.tf
+++ TERRAFORM/01/version.tf
@@ -2,16 +2,16 @@ terraform {
   required_version = "> 1.13"
   required_providers {
     digitalocean = {
-        source = "digitalocean/digitalocean"
-        version = "> 2.0"
+      source  = "digitalocean/digitalocean"
+      version = "> 2.0"
     }
   }
-  cloud { 
-    
-    organization = "piotr_koska_test" 
+  cloud {
 
-    workspaces { 
-      name = "github_action_szkolenie" 
-    } 
-  } 
+    organization = "piotr_koska_test"
+
+    workspaces {
+      name = "github_action_szkolenie"
+    }
+  }
 }
